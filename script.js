30bb9c320230d7b9f1e02daf5348b0bac3beba6a
per-pj <189612052+per-pj@users.noreply.github.com>
2025-10-07 02:37:14 +0900

    デザイン・テキスト・レイアウトのリニューアル、ダークモードの配色に変更、シングルページ化

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const hamburger = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav-list');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active'); // ×に変化
-  navList.classList.toggle('open'); // メニュー開閉
+  hamburger.classList.toggle('active');
+  navList.classList.toggle('open');
 });
